12c72486a2035f80d6ad75a3d4c9f6c44abbc651
pilat <pilat@hp-pilat.fritz.box>
2023-11-03 16:45:01 +0100

    Auto-commit

--- resources/views/layouts/admin-layout.blade.php
+++ resources/views/layouts/admin-layout.blade.php
@@ -1,9 +1,12 @@
 <!-- resources/views/layouts/admin-layout.blade.php -->
 <!DOCTYPE html>
 <html>
+    
 <head>
+    
     <title>@yield('title')</title>
     <!-- Add your CSS and JavaScript links here -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     @yield('content')
